e7cc4511cb4078725389ce0ddf386d523dd9a53e
Z-HNAN <zhn68033@163.com>
2019-12-01 13:32:02 +0800

    feat 增加postCard插件

---
+++ src/components/PostCard/PostCard.tsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import moment, { Moment } from 'moment'
+
+import {
+  Card,
+  Tag,
+} from 'antd-mobile'
+
+import Icon from '../Icon'
+
+import { getWeekday } from '@/utils/momentExtends'
+
+import styles from './PostCard.less'
+
+export interface IProps {
+  /* 招工标题 */
+  title: string
+  /* 开始结束日期 timestamp */
+  startDate: number
+  endDate: number
+  /* 报名人数/总人数 */
+  nowCount: number
+  totalCount: number
+  /* 所属标签 */
+  tags: string[]
+}
+
+const processWeekDate = (startDate: number, endDate: number): string => {
+  const start = moment(startDate)
+  const end = moment(endDate)
+
+  const getDate = (moment: Moment): string => moment.format('MM.DD')
+  return `工作日期 ${getDate(start)}（${getWeekday(start)}）— ${getDate(end)}（${getWeekday(end)}）`
+}
+
+export const PostCard: React.FC<IProps> = props => {
+  const {
+    title,
+    startDate,
+    endDate,
+    nowCount,
+    totalCount,
+    tags,
+  } = props
+
+  const titleDOM = (
+    <div className={styles.cardTitle}>
+      {title}
+    </div>
+  )
+
+  const content = processWeekDate(startDate, endDate)
+
+  const footDOM = (
+    <div className={styles.cardFoot}>
+      <div>
+        报名人数 {`${nowCount} / ${totalCount}`}
+      </div>
+      <div>
+      {
+        tags.map(text => (<Tag className={styles.cardFootTag} small>{text}</Tag>))}
+      </div>
+    </div>
+  )
+
+  return (
+    <Card full>
+      <Card.Header title={titleDOM} />
+      <Card.Body>{content}</Card.Body>
+      <Card.Footer content={footDOM}/>
+    </Card>
+  )
+}
+
