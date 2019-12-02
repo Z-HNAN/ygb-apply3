a2b5a354d9fb11abcc2e9bc5083dfacaf7ec8532
Z-HNAN <zhn68033@163.com>
2019-12-02 16:58:11 +0800

    feat 初始化postInfo页面，完成页面大体搭建

---
+++ src/pages/postInfo/components/InfoHead/index.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+
+import {
+  Tag,
+} from 'antd-mobile'
+
+import styles from './index.less'
+
+export interface IProps {
+  className?: string
+  /**
+   * 标题内容
+   * tags 显示岗位的标签
+   * contact 显示岗位负责人 
+   */
+  content: 'tags' | 'contact'
+  department: string
+  title: string
+  tags?: string[]
+  contactName?: string
+  contactPhone?: number
+}
+
+const InfoHead: React.FC<IProps> = props => {
+  const {
+    className,
+    content,
+    tags = [],
+    department,
+    title,
+    contactName,
+    contactPhone,
+  } = props
+
+  const contentTags = (
+    <div>
+     {
+      tags.map(text => (<Tag className={styles.tag} small>{text}</Tag>))}
+    </div>
+  )
+
+  const contentContact = (
+    <div>
+      <span className={styles.contactName}>{contactName}</span>
+      <span>{contactPhone}</span>
+    </div>
+  )
+
+  const contentMaps = {
+    'tags': contentTags,
+    'contact': contentContact,
+  }
+
+  const avator = department.slice(0, 1)
+
+  return (
+    <div className={`${styles.root} ${className}`}>
+      <div className={styles.avator}>
+        {avator}
+      </div>
+      <div className={styles.description}>
+        <div className={styles.title}>
+          {title}
+        </div>
+        {contentMaps[content]}
+      </div>
+    </div>
+  )
+}
+
+export default InfoHead
--- src/pages/postInfo/postInfo.tsx
+++ src/pages/postInfo/postInfo.tsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import { StickyContainer, Sticky } from 'react-sticky'
+
+import {
+  Tabs,
+} from 'antd-mobile'
+
+import InfoHead from './components/InfoHead'
 
 import styles from './postInfo.less'
 
@@ -6,10 +13,42 @@ export interface IProps {
 
 }
 
+const tabs = [{title: '岗位详情', sub: 'info'},{title: '时间安排', sub: 'time'}]
+
 const PostInfo: React.FC<IProps> = (props) => {
 
+  /* 渲染岗位详情的吊顶 */
+  const renderTabBar = (props: any) => (
+    <Sticky>
+      {({ style }) => <div style={{ ...style, zIndex: 1 }}><Tabs.DefaultTabBar {...props} /></div>}
+    </Sticky>
+  )
+
   return (
-    <div>postInfo</div>
+    <div className={styles.root}>
+      <InfoHead
+        className={styles.head}
+        content='tags'
+        department='义务工作发展部'
+        title='保卫处招募交通志愿者'
+        tags={['助工', '时间灵活']}
+      />
+      <div className={styles.body}>
+        <StickyContainer>
+          <Tabs tabs={tabs} renderTabBar={renderTabBar}>
+            <div className={styles.tabContainer} key='info'>
+              <h2 className={styles.tabContainerTitle}>岗位详情</h2>
+              <p className={styles.tabContainerParagraph}>
+                postConent
+              </p>
+            </div>
+            <div className={styles.tabContainer}  key='time'>
+              Content of second tab
+            </div>
+          </Tabs>
+        </StickyContainer>
+      </div>
+    </div>
   )
 }
 
