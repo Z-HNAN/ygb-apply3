74ec1609ea8ec9dc133a28a9dac1259714f26dd7
Z-HNAN <zhn68033@163.com>
2019-12-02 17:06:58 +0800

    feat 分离taber的path，达到指定页面显示tabbar

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -11,8 +11,17 @@ import { IConnectState } from '@/models/connect.d'
 
 import styles from './BasicLayout.less'
 
+// 需要路由跳转的页面
+const BarRoutes= ['/post', '/schedule', '/me']
+
 const BasicLayout: React.FC<any> = props => {
   const { children, location } = props
+
+  // 不在tabBar中的
+  if (BarRoutes.indexOf(location.pathname) < 0) {
+    return <div className={styles.root}>{children}</div>;
+  }
+
   return (
     <div className={styles.root}>
       <MenuBar pathname={location.pathname} >{children}</MenuBar>
