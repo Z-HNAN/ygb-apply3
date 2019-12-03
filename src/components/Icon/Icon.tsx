392bb5200b55e3dfaa14e8cb1c62629cf79dba40
Z-HNAN <zhn68033@163.com>
2019-12-03 23:17:20 +0800

    feat 增加Icon图标question,并且增加onClick方法

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -9,15 +9,20 @@ export interface IconProps {
   type: IconType
   className?: string
   size?: 'xxs' | 'sm' | 'md' | 'lg'
+  onClick?: () => void
 }
 
 export const Icon: React.FC<IconProps> = props => {
-  const { type, className = '', size = 'md' } = props
+  const { type, className = '', size = 'md', onClick } = props
 
   // other 如果需要透传
 
+  const handleClick = () => {
+    onClick && onClick()
+  }
+
   return (
-    <svg className={`am-icon am-icon-${size} am-icon-${type} ${className}`}>
+    <svg className={`am-icon am-icon-${size} am-icon-${type} ${className}`} onClick={handleClick}>
       <use xlinkHref={`#icon-${type}`} />
     </svg>
   )
