430254a18c49bd1043352e0c07124b81b86146f5
Z-HNAN <zhn68033@163.com>
2019-12-06 11:28:49 +0800

    feat 增加公寓中心岗位

--- src/pages/post/components/Department/index.tsx
+++ src/pages/post/components/Department/index.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { connect } from 'dva'
 
 import { Dispatch, AnyAction } from 'redux'
-
+import {
+  WhiteSpace,
+} from 'antd-mobile'
 import Map from './components/Map'
 
 import styles from './index.less'
@@ -22,8 +24,13 @@ const Department: React.FC<IProps> = props => {
 
   return (
     <div className={styles.root}>
+      <WhiteSpace />
       <div className={styles.map}>
         <Map selectedId={depId} onClick={handleClick}/>
+      </div>
+      <WhiteSpace />
+      <div className={styles.info}>
+        info
       </div>
     </div>
   )
