3fbbe40e0dbbf8c426f0d265078c8ece4609ca08
Z-HNAN <zhn68033@163.com>
2019-12-08 23:06:58 +0800

    fix 修改公寓中心岗位列表的状态

--- src/pages/post/components/Normal/index.tsx
+++ src/pages/post/components/Normal/index.tsx
@@ -9,6 +9,7 @@ import { INormalPost } from '@/models/post'
 
 import {
   WhiteSpace,
+  Progress,
 } from 'antd-mobile'
 
 import PostCard from '@/components/PostCard'
@@ -45,6 +46,7 @@ const Normal: React.FC<IProps> = props => {
   const postCardsDOM = normalPosts.map(normalPost => (
     <React.Fragment key={normalPost.id}>
       <WhiteSpace />
+      {/* <Progress percent={normalPost.nowCount / normalPost.totalCount * 100} position='normal' /> */}
       <PostCard {...normalPost} onClick={() => { handleClickPost(normalPost.id)}} />
     </React.Fragment>
   ))
