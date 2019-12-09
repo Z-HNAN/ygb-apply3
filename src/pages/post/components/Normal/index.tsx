03b4cae4995b91cd66f4ac0dca48edc598e0731a
Z-HNAN <zhn68033@163.com>
2019-12-09 15:13:41 +0800

    feat 上拉追加，下拉刷新demo

--- src/pages/post/components/Normal/index.tsx
+++ src/pages/post/components/Normal/index.tsx
@@ -10,19 +10,24 @@ import { INormalPost } from '@/models/post'
 import {
   WhiteSpace,
   Progress,
+  PullToRefresh,
+  ListView,
 } from 'antd-mobile'
 
 import PostCard from '@/components/PostCard'
 
+import styles from './index.less'
+
 export interface IProps {
   dispatch: Dispatch<AnyAction>
   normalPosts: INormalPost[]
+  loading: boolean
 }
 
 const mapStateToProps = (state: IConnectState) => {
- const { normalPosts } = state.post
   return {
-    normalPosts,
+    normalPosts: state.post.normalPosts,
+    loading: state.loading.models.post,
   }
 }
 
@@ -30,8 +35,25 @@ const Normal: React.FC<IProps> = props => {
   const { 
     dispatch,
     normalPosts,
+    loading,
   } = props
+  const [appending, setAppending] = React.useState(false)
 
+  const [dataSource, setDataSource] = React.useState(new ListView.DataSource({
+    getRowData: (dataBlob: any, sectionID: any, rowID: any) => dataBlob[sectionID][rowID],
+    rowHasChanged: (row1: any, row2: any) => row1 !== row2,
+  }))
+
+  React.useEffect(() => {
+    // 设置dataSource
+    setDataSource(dataSource.cloneWithRows(normalPosts))
+
+    // 标志append结束
+    setAppending(false)
+  }, [normalPosts])
+
+
+  
   const handleClickPost = (id: string) => {
     // 替换岗位详情Id, 拉取工作信息，跳转页面
     dispatch({ type: 'postInfo/changePostInfo', payload: { postInfoId: id, postType: 'normal' }})
@@ -51,9 +73,48 @@ const Normal: React.FC<IProps> = props => {
     </React.Fragment>
   ))
 
+  /**
+   * 滑动到了顶部，准备触发刷新操作
+   */
+  const handleRefresh = () => {
+    dispatch({ type: 'post/fetchNormalPost', payload: { append: false } })
+  }
+
+  /**
+   * 滑动到了底部，准备触发append操作
+   */
+  const handleAppend = () => {
+    setAppending(true)
+    dispatch({ type: 'post/fetchNormalPost', payload: { append: true} })
+  }
+
+  const rowRender = (normalPost: INormalPost) => {
+    return <div children={normalPost.id} style={{border: '1px solid red', height: '100px', width: '100vw'}} />
+  }
+
   return (
-    <div style={{width: '100vw'}}>
-      {postCardsDOM}
+    <div className={styles.root}>
+
+        <ListView
+          className={styles.append}
+          dataSource={dataSource}
+          renderFooter={() => (<div style={{ padding: 30, textAlign: 'center' }}>
+            {appending ? '加载中...' : '加载更多'}
+          </div>)}
+          renderRow={rowRender}
+          onEndReached={handleAppend}
+          pullToRefresh={
+            /* tslint-disable-next-line */
+            <PullToRefresh
+              className={styles.refresh}
+              damping={60}
+              direction='down'
+              refreshing={loading}
+              onRefresh={handleRefresh}
+            />
+          }
+          onEndReachedThreshold={10}
+        />
     </div>
   )
 }
