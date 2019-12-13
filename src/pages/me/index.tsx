6be9ce61180c92934eff2706f1bf4bd3c9a74797
Z-HNAN <zhn68033@163.com>
2019-12-13 11:41:46 +0800

    feat 我的，静态展示页面构建

--- src/pages/me/index.tsx
+++ src/pages/me/index.tsx
@@ -7,6 +7,15 @@ import router from 'umi/router'
 import { IConnectState } from '@/models/connect.d'
 import { Dispatch, AnyAction } from 'redux';
 
+import {
+  WhiteSpace,
+  List,
+} from 'antd-mobile'
+const Item = List.Item
+const Brief = Item.Brief
+
+import styles from './index.less'
+
 export interface OwnProps {
   dispatch: Dispatch<AnyAction>
 }
@@ -21,7 +30,22 @@ const Me: React.FC<OwnProps> = props => {
   const {} = props
 
   return (
-    <div>我的</div>
+    <div className={styles.root}>
+      <div className={styles.photo}>
+        <div className={styles.photoBackground} />
+        <div className={styles.photoImage} />
+      </div>
+      <div className={styles.info}>
+        <div className={styles.studentName}>白勇</div>
+        <div className={styles.studentID}>学号：17051636</div>
+        <div className={styles.studentCollege}>学院：巴巴爸爸吧撒的撒多大杀四方</div>
+      </div>
+      <WhiteSpace />
+      <List>
+        <Item arrow="horizontal" onClick={() => { }}>反馈</Item>
+        <Item arrow="horizontal" onClick={() => { }}>退出登录</Item>
+      </List>
+    </div>
   )
 }
 
