b3ab4f32788a8152f5f99be3a5d8f8d3d120f19f
Z-HNAN <zhn68033@163.com>
2019-12-15 22:23:31 +0800

    feat 增加自定义返回结果的baseURL

--- config/config.dev.ts
+++ config/config.dev.ts
@@ -2,7 +2,8 @@ import { IConfig } from 'umi-types'
 
 const config: IConfig = {
   define: {
-    'process.env.base_url': 'https://mockapi.eolinker.com/vRJhDJJ6a49449fb6d6ebec64f0d81e35cee482ddd17675/',
+    'process.env.base_url': 'https://result.eolinker.com/vRJhDJJ6a49449fb6d6ebec64f0d81e35cee482ddd17675?uri=',
+    // 'process.env.base_url': 'https://mockapi.eolinker.com/vRJhDJJ6a49449fb6d6ebec64f0d81e35cee482ddd17675/',
   }
 }
 
